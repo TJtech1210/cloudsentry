56149b07db75e89c8fa3b716eaef1cf67c411774
Thomas Marshall <thomas1012m@gmail.com>
2026-01-17 23:33:57 -0800

    Integrate boto3 to check for IAM users
    Updated cloudsentry.py to use boto3 for IAM user data retrieval and modified findings logic.

--- cloudsentry.py
+++ cloudsentry.py
@@ -1,5 +1,19 @@
+# TODO: Replace fake findings with IAM user data using boto3
+import boto3
 import sys
-findings = ["PII", "MFA", "Weak Credentials", "High risk: Weak Credentials"] 
+iam = boto3.client("iam")
+
+response = iam.list_users()
+users = response.get("Users", [])
+
+findings = []
+
+if users:
+    findings.append("High risk: IAM users exist in account")
+else:
+    findings.append("No IAM users found")
+
+
 high_risk_exists = False
 
 for finding in findings:
@@ -11,6 +25,9 @@ if high_risk_exists:
   sys.exit(1)
 else:
   sys.exit(0)
+
+aws configure
+
 
 
 
