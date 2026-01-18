c5eee1add61930d97435eb42ca0fcf38587f5038
Thomas Marshall <thomas1012m@gmail.com>
2026-01-17 17:54:22 -0800

    Simplify condition check for high risk findings

--- cloudsentry.py
+++ cloudsentry.py
@@ -1,10 +1,11 @@
 import sys
 high_risk_exists = False
 
-if high_risk_exists == True:
+if high_risk_exists:
   print("High risk finding detected")
   sys.exit(1)
 else: 
+  print("No high risk findings")
   sys.exit(0)
 
 
