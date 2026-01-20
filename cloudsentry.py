daa962d9c52ea92d270e6a92b05a54800f908d2d
Thomas Marshall <thomas1012m@gmail.com>
2026-01-20 14:40:06 -0800

    Update cloudsentry.py

--- cloudsentry.py
+++ cloudsentry.py
@@ -24,7 +24,7 @@ if USE_MOCK:
         {
             "UserName": "test-admin",
             "HasAdminAccess": True,
-            "HasMFA": False
+            "HasMFA": True
         }
     ]
 else:
@@ -43,7 +43,7 @@ if USE_MOCK:
                     "FromPort": 22,
                     "ToPort": 22,
                     "IpProtocol": "tcp",
-                    "IpRanges": [{"CidrIp": "0.0.0.0/0"
+                    "IpRanges": [{"CidrIp": "10.0.0.0/32"
 }]
                 }
             ]
