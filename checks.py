f6fd37da628e336c6a44dc2cbe1c05a5b9334536
Thomas Marshall <thomas1012m@gmail.com>
2026-02-17 11:49:56 -0600

    Create checks.py with modular security check functions for IAM, EC2, S3, and RDS resources

---
+++ checks.py
@@ -0,0 +1,29 @@
+def check_iam():
+    """Perform IAM security checks"""
+    # Code to check IAM policies and roles
+    pass
+
+
+def check_ec2():
+    """Perform EC2 security checks"""
+    # Code to check EC2 security
+    pass
+
+
+def check_s3():
+    """Perform S3 security checks"""
+    # Code to check S3 bucket policies
+    pass
+
+
+def check_rds():
+    """Perform RDS security checks"""
+    # Code to check RDS instance settings
+    pass
+
+
+if __name__ == '__main__':
+    check_iam()
+    check_ec2()
+    check_s3()
+    check_rds()
